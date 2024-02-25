2d45571b95f423f4ceab0d0ce00b8b459a440559
Stephen English <steve@secomputing.co.uk>
2024-02-25 08:53:44 -0800

    Tidy tidy tidy. Receive unpadding

--- led1248.py
+++ led1248.py
@@ -4,8 +4,10 @@ from enum import Enum
 from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
 import logging
 from text import text_payload
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
+
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class SCROLL(Enum):
     SCROLLSTATIC = 1
@@ -17,15 +19,14 @@ class SCROLL(Enum):
     SCROLLPICTURE = 7
     SCROLLLASER = 8
 
-def hexy(b):
-    return ' '.join('{:02X}'.format(a) for a in b)
+class GRAPHIC_TYPE(Enum):
+    TEXT = 2
+
 
 async def search():
     logger.info("scanning for 5 seconds, please wait...")
 
-    devices = await BleakScanner.discover(
-        return_adv=True, cb=dict(use_bdaddr=False)
-    )
+    devices = await BleakScanner.discover(return_adv=True, cb=dict(use_bdaddr=False))
 
     for k, v in devices.items():
         d, a = v
@@ -35,12 +36,13 @@ async def search():
         logger.info("-" * len(str(d)))
         logger.info(a)
 
+
 async def blop():
     UUID = "2BD223FA-4899-1F14-EC86-ED061D67B468"
     device = await BleakScanner.find_device_by_address(UUID)
 
     if device is None:
-        logger.warn(f"UUID {UUID} not found")
+        logger.error(f"UUID {UUID} not found")
         sys.exit(1)
 
     def handle_disconnect(_: BleakClient):
@@ -48,62 +50,85 @@ async def blop():
         # cancelling all tasks effectively ends the program
         for task in asyncio.all_tasks():
             task.cancel()
-
-    def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
-        logger.info("received:" + hexy(data))
-
-    async def send(client, char, bytes):
-        payload = len(bytes).to_bytes(2, "big") + bytes
+    
+    def pad(payload):
         padded = bytearray()
         for b in payload:
             if b == 1 or b == 2 or b == 3:
                 padded.append(0x02)
-                padded.append(b+4)
+                padded.append(b + 4)
             else:
                 padded.append(b)
-
-        cmd = b"\x01" + padded + b"\x03"
-        logging.info(f"Sending: {hexy(cmd)}")
-        await client.write_gatt_char(char, cmd, response=False)
-        logging.info("Sent. Sleeping")
-        await asyncio.sleep(0.1)
+        return padded
     
+    def unpad(data):
+        payload = bytearray()
+        i = 0
+        while i < len(data):
+            if data[i] == 0x02:
+                payload.append(data[i+1] - 4)
+                i += 2
+            else:
+                payload.append(data[i])
+                i += 1
+        return payload
+
+    def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
+        assert data[0] == 0x01, "Invalid start byte"
+        assert data[-1] == 0x03, "Invalid end byte"
+        payload = unpad(data[1:-1])
+        logger.info("received:" + payload.hex())
+
+    async def send(client, char, bytes):
+        payload = len(bytes).to_bytes(2, "big") + bytes
+        padded = pad(payload)
+        cmd = b"\x01" + padded + b"\x03"
+        logging.debug(f"Sending: {cmd.hex()}")
+        await client.write_gatt_char(char, cmd, response=False)
+        await asyncio.sleep(0.1)
+
     def split_payload(payload):
         LEN = 128
-        return [payload[start:start+LEN] for start in range(0, len(payload), LEN)]
-    
+        return [payload[start : start + LEN] for start in range(0, len(payload), LEN)]
+
     def build_packet(payload_size, packet_id, packet):
-        return b"".join([
-            # 'tis not ours to wonder why
-            b"\x00",
-            # total payload size
-            payload_size.to_bytes(2, "big"),
-            # packet id
-            packet_id.to_bytes(2, "big"),
-            # packet size
-            len(packet).to_bytes(1, "big"),
-            packet,
-        ])
-    
+        return b"".join(
+            [
+                # 'tis not ours to wonder why
+                b"\x00",
+                # total payload size
+                payload_size.to_bytes(2, "big"),
+                # packet id
+                packet_id.to_bytes(2, "big"),
+                # packet size
+                len(packet).to_bytes(1, "big"),
+                packet,
+            ]
+        )
+
     def checksum(packet):
         c = 0
         for x in packet:
             c ^= x
         return c.to_bytes(1, "big")
-    
-    async def send_stream(client, char, payload):
-        packets = [build_packet(len(payload), i, data) for i, data in enumerate(split_payload(payload))]
+
+    async def send_stream(client, char, stream_type, payload):
+        packets = [
+            build_packet(len(payload), i, data)
+            for i, data in enumerate(split_payload(payload))
+        ]
         for packet in packets:
             await send(
                 client,
                 char,
-                b"".join([
-                    # Command - 2 means text apparantly
-                    b"\x02",
-                    packet,
-                    # Remember the checksum
-                    checksum(packet),
-                ]),
+                b"".join(
+                    [
+                        stream_type.value.to_bytes(1, "big"),
+                        packet,
+                        # Remember the checksum
+                        checksum(packet),
+                    ]
+                ),
             )
 
     async with BleakClient(device, disconnected_callback=handle_disconnect) as client:
@@ -119,8 +144,9 @@ async def blop():
 
             async def scroll(dir):
                 await send(client, char, b"\x06" + dir.value.to_bytes(1, "big"))
-            # await scroll(SCROLL.SCROLLLEFT)
-            await send_stream(client, char, text_payload("Hello World", "red", 16))
+
+            await scroll(SCROLL.SCROLLLEFT)
+            await send_stream(client, char, GRAPHIC_TYPE.TEXT, text_payload("Hello World", "yellow", 16))
             await asyncio.sleep(1)
 
             logging.info("Done")
@@ -128,8 +154,9 @@ async def blop():
             print(ex)
             raise ex
 
+
 if __name__ == "__main__":
-    #asyncio.run(search())
+    # asyncio.run(search())
     try:
         asyncio.run(blop())
     except asyncio.CancelledError:
