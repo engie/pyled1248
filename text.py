d59a83f26b699d1658797f1c768c2c89a9750aaa
Stephen English <steve@secomputing.co.uk>
2024-02-26 06:19:46 -0800

    Use a file path calc to load the font

--- text.py
+++ text.py
@@ -1,4 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
+import os
+
+def get_font_path():
+    dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(dir, "spleen-8x16.pil")
 
 def render_text(text, color, height):
     img = Image.new("RGB", (400, 16))
@@ -6,7 +11,7 @@ def render_text(text, color, height):
         (0, 0),
         text,
         ImageColor.getrgb(color),
-        font = ImageFont.load_path("spleen-8x16.pil"),
+        font = ImageFont.load(get_font_path())
     )
     x, y, full_width, full_height = img.getbbox()
     return img.crop((x, 0, full_width, 16))
